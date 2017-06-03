ba21f81ee30bb3e0d126aa21305f3ddce4036d01
salehmashal <saleh-jaz1@hotmail.com>
2017-06-03 21:42:23 +0300

    application.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '80';
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
 /**
